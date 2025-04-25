dfabd6e6c1f56e037eaf8a7ec3c1bcd5db6c641f
ileztom <91593668+ileztom@users.noreply.github.com>
2025-04-25 18:50:28 +0300

    Delete start.ps1

--- start.ps1
+++
@@ -1,9 +0,0 @@
-.\config.cmd `
-    --url $env:REPO_URL `
-    --token $env:RUNNER_TOKEN `
-    --name $env:RUNNER_NAME `
-    --work $env:RUNNER_WORKDIR `
-    --unattended `
-    --replace
-
-.\run.cmd
